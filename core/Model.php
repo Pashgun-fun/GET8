c70ebde4c9e0be0f4f084a6e053d5ec63df1a1d2
Pavel <antonov.pavel.00@yandex.ru>
2021-12-05 13:15:45 +0300

    комментарий

--- core/Model.php
+++ core/Model.php
@@ -18,21 +18,48 @@ class Model
      */
     public function appendData(string $rangeCells, array $valuesCells)
     {
+        /**
+         * Создаём подключение к гугл клиенту, для работы с его сервисами
+         */
         $client = new Google_Client();
+        /**
+         * Подключаемся к названию проекта
+         */
         $client->setApplicationName('Google Sheets and PHP');
         $client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
+        /**
+         * Задаём тип доступа
+         */
         $client->setAccessType('offline');
+        /**
+         * Подключаем файл с параметрами подключения
+         */
         $client->setAuthConfig(__DIR__ . "/../credentials.json");
 
         $service = new Google_Service_Sheets($client);
+        /**
+         * ID для подключения
+         */
         $spreadSheetId = "192WTENvUnkN7dE5DLsX_hha0Sl4Zb-cYhFMp7rcK08o";
+        /**
+         * Переменная куда будут заноситься интервал ячеек для работы
+         */
         $range = $rangeCells;
+        /**
+         * Переменная для значений, которые будут заноситься
+         */
         $values = [$valuesCells];
 
+        /**
+         * Отправляем данные в саму таблицу
+         */
         $body = new Google_Service_Sheets_ValueRange([
             'values' => $values
         ]);
 
+        /**
+         * Параметры для занесения данных
+         */
         $params = [
             'valueInputOption' => 'RAW'
         ];
